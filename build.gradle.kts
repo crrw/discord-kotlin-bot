7d113ccb994ac14891adf7e628ea1b35d9812443
aasish23 <47400245+aasish23@users.noreply.github.com>
2023-03-13 21:48:28 -0400

    revert changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,10 +31,6 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-
 tasks.withType<Jar> {
     // Otherwise you'll get a "No main manifest attribute" error
     manifest {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,12 @@
 import io.github.cdimascio.dotenv.Dotenv
-import listeners.RoleAssigner
+import listeners.EventListener
 import service.Bot
 
 fun main(args: Array<String>) {
 
-    val dotenv: Dotenv = Dotenv.configure().filename(".env").load()
-    val TOKEN: String = dotenv.get("TOKEN")
+    val TOKEN: String = System.getenv("TOKEN")
 
     var bot = Bot(token = TOKEN)
 
-    bot.shardManager.addEventListener(RoleAssigner())
+    bot.shardManager.addEventListener(EventListener())
 }
