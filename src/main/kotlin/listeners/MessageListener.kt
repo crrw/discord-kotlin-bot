25e138e63eb3935c9cb807058501eccf22170b7d
Aasish <47400245+crrw@users.noreply.github.com>
2023-03-25 13:50:54 -0400

    Custom purge amount (#5)
    * refactor message listener to use sets
    
    * add a way to purge a custom number of messages
    
    ---------
    
    Co-authored-by: aasish23 <47400245+aasish23@users.noreply.github.com>

--- src/main/kotlin/listeners/MessageListener.kt
+++ src/main/kotlin/listeners/MessageListener.kt
@@ -12,23 +12,27 @@ class MessageListener : ListenerAdapter() {
     private val setOfAdmins = setOf(1039600188446220359, 148646710787178496)
 
     /**
-     * purge 10 messages at a time in any channel where `purge` is typed
+     * purge some number of messages at a time in any channel where `purge` is typed
      */
     override fun onMessageReceived(event: MessageReceivedEvent) {
         val channelId = event.message.channel.idLong
         val user: User = event.author
         val userId: Long = event.author.idLong
 
-        if (event.message.contentRaw.equals("purge", true) &&
-            (setOfAdmins.contains(event.member?.idLong))
-        ) {
-            val channel = event.jda.getTextChannelById(channelId)
+        val currentMessage = event.message.contentRaw.split(" ")
 
+        if (currentMessage[0].equals("purge", true) &&
+            setOfAdmins.contains(event.member?.idLong)
+        ) {
+            val numberOfMessagesToDelete = currentMessage[1]
+
+            println("purge")
             println("userId=$userId")
             println("user=$user")
 
-            requireNotNull(channel) { "channel must not be null" }
-            channel.history.retrievePast(10).queue { message ->
+            val channel = event.jda.getTextChannelById(channelId)
+            requireNotNull(channel)
+            channel.history.retrievePast(numberOfMessagesToDelete.toInt()).queue { message ->
                 channel.purgeMessages(message)
             }
         }
