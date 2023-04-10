a69351b3a98e1ccc24748fe5d845e3ddd16066a9
aasish23 <47400245+aasish23@users.noreply.github.com>
2023-04-10 19:18:21 -0400

    remove local testing

--- src/main/kotlin/config/DatabaseConfig.kt
+++ src/main/kotlin/config/DatabaseConfig.kt
@@ -8,17 +8,17 @@ import java.sql.Connection
 
 class DatabaseConfig {
 
-//    val TOKEN: String = System.getenv("TOKEN")
-//    val jdbcUrl = System.getenv("url")
-//    val userName = System.getenv("user")
-//    val password = System.getenv("password")
+    val TOKEN: String = System.getenv("TOKEN")
+    val jdbcUrl = System.getenv("url")
+    val userName = System.getenv("user")
+    val password = System.getenv("password")
 
     //    for local testing
-    private val env: Dotenv = Dotenv.configure().filename(".env").load()
-    private val TOKEN = env.get("TOKEN")
-    private val url = env.get("url")
-    private val user = env.get("user")
-    private val password = env.get("password")
+//    private val env: Dotenv = Dotenv.configure().filename(".env").load()
+//    private val TOKEN = env.get("TOKEN")
+//    private val url = env.get("url")
+//    private val user = env.get("user")
+//    private val password = env.get("password")
 
     fun startBot() {
 
